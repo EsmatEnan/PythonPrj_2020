426e88f47fe5ccdac7b43699f105dcfee101f37b
masawdah <masawdah@gmail.com>
2020-06-25 08:31:46 +0100

    fixed the clipped raster origin

--- py_proj_crop.py
+++ py_proj_crop.py
@@ -3,71 +3,59 @@ import gdal
 import ogr
 
 
-<<<<<<< HEAD
 raster = r"E:\Munster\python\final project\Data\gm_lc_v3_1_2.tif"
 rast_data_source = gdal.Open(raster)
 
 ####### Cropping the raster #######
 
-reproj_shp = r"E:\Munster\python\final project\movebank\goose\outputs\pointsPrj.shp"
-=======
-raster = os.path.join(work_dir, 'gm_lc_v3_1_2.tif')
-raster_ds = gdal.Open(raster)
-
-####### Cropping the raster #######
-
-reproj_shp = os.path.join(work_dir, 'reproj', 'points_rpj.shp')
->>>>>>> 5c93bf3faebc5df1d0c35122700b042c14e1c5f8
+reproj_shp = r"E:\Munster\python\final project\movebank\goose\points.shp"
 driver = ogr.GetDriverByName('ESRI Shapefile')
 shape = driver.Open(reproj_shp, 0)
 shp_lyr = shape.GetLayer(0)
 
-#Extent of Shapefile
+#BoundingBox of Shapefile
 x_min, x_max, y_min, y_max = shp_lyr.GetExtent()
-print("Shapefile bounding box coordinates:", x_min, x_max, y_min, y_max)
+print("GetExtent returned", x_min, x_max, y_min, y_max)
 
 
 # Getting the object to translate coordinates to indices
-gt = raster_ds.GetGeoTransform()
+gt = rast_data_source.GetGeoTransform()
 inv_gt = gdal.InvGeoTransform(gt)
 
 # Converting BBox coordinates to raster indices
-buffer = 1000
-
-x1, y1= gdal.ApplyGeoTransform(inv_gt, x_min - buffer, y_min - buffer)
-x2, y2= gdal.ApplyGeoTransform(inv_gt, x_max + buffer, y_max + buffer)
+x1, y1= gdal.ApplyGeoTransform(inv_gt, x_min, y_min)
+x2, y2= gdal.ApplyGeoTransform(inv_gt, x_max, y_max)
 
 # Turning indices into integers variables
-x1 = int(round(x1))
-y1 = int(round(y1))
-x2 = int(round(x2))
-y2 = int(round(y2))
-print("Lower left corner indice:", x1, y1, '\t', "Upper Right Corner indice:", x2, y2)
+x1 = int(x1)
+y1 = int(y1)
+x2 = int(x2)
+y2 = int(y2)
+print("Lower left corner:", x1, y1, '\t', "Upper Right Corner:", x2, y2)
+
+buffer = 2000
 
 #Clipping Settings
-out_columns = x2 - x1
-out_rows = y1 - y2
-print("output raster extent is:", out_columns, out_rows)
+out_columns = x2 - x1 + buffer
+out_rows = y1 - y2 + buffer
 
 #Output Clipped Raster
-<<<<<<< HEAD
 clipped_raster = r"E:\Munster\python\final project\movebank\goose\outputs\clipped_raster.tif"
-=======
-clipped_raster = os.path.join(work_dir,'clipped_gm_lc.tif')
->>>>>>> 5c93bf3faebc5df1d0c35122700b042c14e1c5f8
 driver = gdal.GetDriverByName("GTiff")
+print("Out size", out_columns, out_rows)
 out_ds = driver.Create(clipped_raster, out_columns, out_rows, 1, gdal.GDT_UInt32)
-out_ds.SetProjection(raster_ds.GetProjection())
+projection = rast_data_source.GetProjection()
+out_ds.SetProjection(projection)
 
 # Applying Geotransform
 out_gt = list(gt)
-out_gt[0] = x_min - 2*buffer
-out_gt[3] = y_max + 2*buffer
+out_gt[0] = x_min - 2 
+out_gt[3] = y_max + 2
 print(out_gt)
 out_ds.SetGeoTransform(out_gt)
 
 # Writing new raster from the input raster
-in_raster = raster_ds.GetRasterBand(1)
+in_raster = rast_data_source.GetRasterBand(1)
 out_raster = out_ds.GetRasterBand(1)
 data = in_raster.ReadAsArray(x1, y2, out_columns, out_rows)
 out_raster.WriteArray(data)
